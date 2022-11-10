44ca26ef63c4cedc455cd71970fa29c1242bd16b
Gregory <109724010+Gregory900@users.noreply.github.com>
2022-11-10 18:11:42 +0100

    Add files via upload

---
+++ Projekt Grzegorz Waligórski/js/script.js
@@ -0,0 +1,45 @@
+const name="Grzegorz";
+const age = 32;
+
+console.log(`Mam na imie ${name} i mam ${age} lata`);
+console.log(name, age);
+
+const score = 15%2;
+
+console.log(score)
+
+let Num1=8;
+++Num1;
+++Num1;
+
+console.log(Num1)
+
+let x = 5;
+let y = 10;
+
+console.log(x*=y);
+console.log(x/=y);
+
+const tab = [1, 2, 3, 4];
+
+for (i=0; i<tab.length;i++){
+    console.log(tab[i])
+}
+
+function add (b, c, d){
+    console.log(b+c*d);
+}
+
+
+add(5,4,6)
+
+if (tab.length>5){
+    console.log(true)
+}
+else console.log(false)
+
+
+
+
+
+
